a14fcecf6ab39fd3b3dd086b9250da770266b492
Adrián Arroyo Calle <adrian.arroyocalle@gmail.com>
2021-02-24 21:59:57 +0100

    Player in Rust

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,11 @@
-fn init() {
+use gdnative::prelude::*;
+
+mod player;
+
+fn init(handle: InitHandle) {
     println!("Bardulia 0.0.1");
+
+    handle.add_class::<player::Player>();
 }
 
 godot_init!(init);
---
+++ src/player.rs
@@ -0,0 +1,55 @@
+use gdnative::prelude::*;
+use gdnative::api::AnimationPlayer;
+
+#[derive(NativeClass)]
+#[inherit(Spatial)]
+pub struct Player {
+    animation: Ref<AnimationPlayer, Unique>,
+}
+
+#[gdnative::methods]
+impl Player {
+    fn new(owner: &Spatial) -> Self {
+        Player {
+            animation: Self::get_animation_player(owner)
+        }
+    }
+
+    fn get_animation_player(owner: &Spatial) -> Ref<AnimationPlayer, Unique> {
+        let animation = owner.get_node("AnimationPlayer").unwrap();
+        unsafe {
+            animation
+                .assume_unique()
+                .cast::<AnimationPlayer>()
+                .unwrap()
+        }
+    } 
+
+    #[export]
+    fn _ready(&self, _owner: &Spatial) {
+        self.animation.play("Idle", -1.0, 1.0, false);
+    }
+
+    #[export]
+    fn _process(&self, owner: &Spatial, delta: f64) {
+        let delta = delta as f32;
+        let input = Input::godot_singleton();
+        if Input::is_action_pressed(&input, GodotString::from_str("move_forward")) {
+            self.animation.play("Run", -1.0, 1.0, false);
+            let rot_x = owner.rotation().x;
+            let rot_z = owner.rotation().z;
+            let vector = Vector3::new(delta*2.0f32*rot_x.sin(), 0.0, delta*2.0f32*rot_z.cos());
+            owner.translate(vector);
+        } else {
+            self.animation.play("Idle", -1.0, 1.0, false);
+        }
+
+        if Input::is_action_pressed(&input, GodotString::from_str("rotate_right")) {
+            owner.rotate_y((delta*-2.0f32).into());
+        }
+
+        if Input::is_action_pressed(&input, GodotString::from_str("rotate_left")) {
+            owner.rotate_y((delta*2.0f32).into());
+        }
+    }
+}
