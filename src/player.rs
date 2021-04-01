f5a74fd2aac47635f949fc5a25fad951b90d9d4e
Adrián Arroyo Calle <adrian.arroyocalle@gmail.com>
2021-04-01 21:44:35 +0200

    GridMap & Collisions

--- src/player.rs
+++ src/player.rs
@@ -1,25 +1,23 @@
 use gdnative::prelude::*;
 use gdnative::api::AnimationPlayer;
 
-const PLAYER_SPEED: f32 = 4.0;
+const GRAVITY: f32 = 1.0;
+
+const PLAYER_SPEED: f32 = 1.0;
 const ROTATE_SPEED: f32 = 3.0;
 
 #[derive(NativeClass)]
-#[inherit(Spatial)]
-pub struct Player {
-    animation: Ref<AnimationPlayer, Unique>,
-}
+#[inherit(KinematicBody)]
+pub struct Player;
 
 #[gdnative::methods]
 impl Player {
-    fn new(owner: &Spatial) -> Self {
-        Player {
-            animation: Self::get_animation_player(owner)
-        }
+    fn new(_owner: &KinematicBody) -> Self {
+        Player
     }
 
-    fn get_animation_player(owner: &Spatial) -> Ref<AnimationPlayer, Unique> {
-        let animation = owner.get_node("AnimationPlayer").unwrap();
+    fn get_animation_player(&self, owner: &KinematicBody) -> Ref<AnimationPlayer, Unique> {
+        let animation = owner.get_node("./AnimationPlayer").unwrap();
         unsafe {
             animation
                 .assume_unique()
@@ -29,22 +27,24 @@ impl Player {
     } 
 
     #[export]
-    fn _ready(&self, _owner: &Spatial) {
-        self.animation.play("Idle", -1.0, 1.0, false);
+    fn _ready(&self, owner: &KinematicBody) {
+        let animation = self.get_animation_player(owner);
+        animation.play("Idle", -1.0, 1.0, false);
     }
 
     #[export]
-    fn _process(&self, owner: &Spatial, delta: f64) {
+    fn _physics_process(&self, owner: &KinematicBody, delta: f64) {
+        let animation = self.get_animation_player(owner);
         let delta = delta as f32;
         let input = Input::godot_singleton();
+
         if Input::is_action_pressed(&input, GodotString::from_str("move_forward")) {
-            self.animation.play("Run", -1.0, 1.0, false);
-            let rot_x = owner.rotation().x;
-            let rot_z = owner.rotation().z;
-            let vector = Vector3::new(delta*PLAYER_SPEED*rot_x.sin(), 0.0, delta*PLAYER_SPEED*rot_z.cos());
-            owner.translate(vector);
+            animation.play("Run", -1.0, 1.0, false);
+            let rot = owner.rotation().y;
+            let vector = Vector3::new(PLAYER_SPEED*rot.sin(), -GRAVITY, PLAYER_SPEED*rot.cos());
+            owner.move_and_slide(vector, Vector3::new(0.0, 1.0, 0.0), false, 4, 0.78, true);
         } else {
-            self.animation.play("Idle", -1.0, 1.0, false);
+            animation.play("Idle", -1.0, 1.0, false);
         }
 
         if Input::is_action_pressed(&input, GodotString::from_str("rotate_right")) {
